7652f4709a92c2b8f2b35bbc7befc269af51beae
Rafael de figueiredo Lopes <rafaelflopes18@gmail.com>
2022-08-19 16:18:55 -0300

    iniciando o Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="meals">
+      <Header />
       <span className="logo">TRYBE</span>
       <object
         className="rocksGlass"
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <h2>Header</h2>
+  );
+}
+export default Header;
