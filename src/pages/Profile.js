5ddb0d31e1ea7af383fdcf9879e634765c289ec1
Peterson <petersonpaganelli@outlook.com>
2022-08-25 16:17:45 -0300

    Requisitos 60 e 61

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+
   const getEmail = () => {
     const text = localStorage.getItem('user');
     const email = JSON.parse(text);
     return email.email;
   };
+
+  const goToDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const goToFavorites = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header />
@@ -19,18 +36,21 @@ function Profile() {
       <button
         type="button"
         data-testid="profile-logout-btn"
+        onClick={ logout }
       >
         Logout
       </button>
       <button
         type="button"
         data-testid="profile-done-btn"
+        onClick={ goToDoneRecipes }
       >
         Done Recipes
       </button>
       <button
         type="button"
         data-testid="profile-favorite-btn"
+        onClick={ goToFavorites }
       >
         Favorite Recipes
       </button>
