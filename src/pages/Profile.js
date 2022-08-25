b75965f42cefb180bd16c4bdf17c3ed7d1f02557
Peterson <petersonpaganelli@outlook.com>
2022-08-25 16:00:33 -0300

    Req57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,29 @@ function Profile() {
   return (
     <div>
       <Header />
+      <h1
+        data-testid="profile-email"
+      >
+        email
+      </h1>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
       <Footer />
     </div>);
 }
