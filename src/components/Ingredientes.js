742f57c079e52f949cd147e7f7d74be5a190f90b
Leonardo Zanette <leonardobzanette@gmail.com>
2022-08-30 18:57:24 -0300

    Requisito 37 - DONE

---
+++ src/components/Ingredientes.js
@@ -0,0 +1,69 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import receitasContext from '../Context/ReceitasContext';
+
+function Ingredientes() {
+  const { recipeDetail } = useContext(receitasContext);
+  const history = useHistory();
+  const { pathname } = history.location;
+  const splited = pathname.split('/');
+
+  const ingredient = () => {
+    if (splited[1] === 'foods') {
+      const magicIngre1 = Object.keys(recipeDetail.meals[0]).indexOf(
+        'strIngredient1',
+      );
+      const magicIngre2 = Object.keys(recipeDetail.meals[0]).indexOf(
+        'strIngredient20',
+      );
+      return Object.values(recipeDetail.meals[0]).filter(
+        (a, i) => i <= magicIngre2 && i >= magicIngre1,
+      );
+    }
+    const magicDrink1 = Object.keys(recipeDetail.drinks[0]).indexOf(
+      'strIngredient1',
+    );
+    const magicDrink2 = Object.keys(recipeDetail.drinks[0]).indexOf(
+      'strIngredient15',
+    );
+    return Object.values(recipeDetail.drinks[0]).filter(
+      (a, i) => i <= magicDrink2 && i >= magicDrink1,
+    );
+  };
+  const measure = () => {
+    if (splited[1] === 'foods') {
+      const magicIngre1 = Object.keys(recipeDetail.meals[0]).indexOf(
+        'strMeasure1',
+      );
+      const magicIngre2 = Object.keys(recipeDetail.meals[0]).indexOf(
+        'strMeasure20',
+      );
+      return Object.values(recipeDetail.meals[0]).filter(
+        (a, i) => i <= magicIngre2 && i >= magicIngre1,
+      );
+    }
+    const magicDrink1 = Object.keys(recipeDetail.drinks[0]).indexOf(
+      'strMeasure1',
+    );
+    const magicDrink2 = Object.keys(recipeDetail.drinks[0]).indexOf(
+      'strMeasure15',
+    );
+    return Object.values(recipeDetail.drinks[0]).filter(
+      (a, i) => i <= magicDrink2 && i >= magicDrink1,
+    );
+  };
+
+  const ingredientes = ingredient().filter((a) => a !== null && a !== '');
+  const medidas = measure().filter((a) => a !== null && a !== '');
+
+  return (
+    <ul>
+      {ingredientes.map((a, i) => (
+        <li key={ i } data-testid={ `${i}-ingredient-step` }>
+          {`${a} ${medidas[i]}`}
+        </li>
+      ))}
+    </ul>);
+}
+
+export default Ingredientes;
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,97 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../App.css';
+import Ingredientes from '../components/Ingredientes';
+import receitasContext from '../Context/ReceitasContext';
 
 function RecipeInProgress() {
-  const [recipeProgress, setRecipeProgress] = useState([]);
+  const { recipeDetail, setRecipeDetail } = useContext(receitasContext);
   const history = useHistory();
   const { pathname } = history.location;
   const splited = pathname.split('/');
-  const foodDrinks = splited[1];
-  const idRecipes = splited[2];
+
+  const getFoodDetail = async () => {
+    if (splited[1] === 'foods') {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${splited[2]}`);
+      const dataResponse = await response.json();
+      setRecipeDetail(dataResponse);
+    } else {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${splited[2]}`);
+      const dataResponse = await response.json();
+      setRecipeDetail(dataResponse);
+    }
+  };
 
   useEffect(() => {
-    const getRecipe = async () => {
-      let endPoint = '';
-      if (foodDrinks === 'foods') {
-        endPoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idRecipes}`;
-      } else {
-        endPoint = `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipes}`;
-      }
-
-      const responseApi = await fetch(endPoint);
-      const dataApi = await responseApi.json();
-      console.log('Resposta API e: ', dataApi);
-      setRecipeProgress(dataApi);
-    };
-    getRecipe().catch(console.error);
+    getFoodDetail();
   }, []);
-
-  const ingredient = () => {
-    console.log('RecipeProgress     ', recipeProgress);
-    if (foodDrinks === 'foods') {
-      const magicIngre1 = Object.keys(recipeProgress.meals[0]).indexOf(
-        'strIngredient1',
-      );
-      const magicIngre2 = Object.keys(recipeProgress.meals[0]).indexOf(
-        'strIngredient20',
-      );
-      // getDrinks();
-      return Object.values(recipeProgress.meals[0]).filter(
-        (a, i) => i <= magicIngre2 && i >= magicIngre1,
-      );
-    }
-    const magicDrink1 = Object.keys(recipeProgress.drinks[0]).indexOf(
-      'strIngredient1',
-    );
-    const magicDrink2 = Object.keys(recipeProgress.drinks[0]).indexOf(
-      'strIngredient15',
-    );
-    // getMeals();
-    return Object.values(recipeProgress.drinks[0]).filter(
-      (a, i) => i <= magicDrink2 && i >= magicDrink1,
-    );
-  };
-
-  const measure = () => {
-    if (foodDrinks === 'foods') {
-      const magicIngre1 = Object.keys(recipeProgress.meals[0]).indexOf(
-        'strMeasure1',
-      );
-      const magicIngre2 = Object.keys(recipeProgress.meals[0]).indexOf(
-        'strMeasure20',
-      );
-      return Object.values(recipeProgress.meals[0]).filter(
-        (a, i) => i <= magicIngre2 && i >= magicIngre1,
-      );
-    }
-    const magicDrink1 = Object.keys(recipeProgress.drinks[0]).indexOf(
-      'strMeasure1',
-    );
-    const magicDrink2 = Object.keys(recipeProgress.drinks[0]).indexOf(
-      'strMeasure15',
-    );
-    return Object.values(recipeProgress.drinks[0]).filter(
-      (a, i) => i <= magicDrink2 && i >= magicDrink1,
-    );
-  };
-
-  const ingredientes = ingredient().filter((a) => a !== null && a !== '');
-  const medidas = measure().filter((a) => a !== null && a !== '');
 
   return (
     <div>
       <h1>RecipeInProgress</h1>
-      {foodDrinks === 'foods' ? (
-        recipeProgress.meals && (
+      {splited[1] === 'foods' ? (
+        recipeDetail.meals && (
           <div>
             <img
               data-testid="recipe-photo"
-              src={ recipeProgress.meals[0].strMealThumb }
+              src={ recipeDetail.meals[0].strMealThumb }
               width="400px"
               alt=""
             />
-            <h2 data-testid="recipe-title">{recipeProgress.meals[0].strMeal}</h2>
+            <h2 data-testid="recipe-title">{recipeDetail.meals[0].strMeal}</h2>
             <button
               type="button"
               data-testid="share-btn"
@@ -105,19 +52,13 @@ function RecipeInProgress() {
               Favoritar
             </button>
             <h4 data-testid="recipe-category">
-              {recipeProgress.meals[0].strCategory}
+              {recipeDetail.meals[0].strCategory}
             </h4>
 
-            {/* <ul>
-              {ingredientes.map((a, i) => (
-                <li key={ i } data-testid={ `${i}-ingredient-step` }>
-                  {`${a} ${medidas[i]}`}
-                </li>
-              ))}
-            </ul> */}
+            { recipeDetail.meals && <Ingredientes />}
 
             <h5 data-testid="instructions">
-              {recipeProgress.meals[0].strInstructions}
+              {recipeDetail.meals[0].strInstructions}
             </h5>
 
             <button
@@ -129,49 +70,44 @@ function RecipeInProgress() {
           </div>
         )
       ) : (
-        <div>
-          {/* <img
-                data-testid="recipe-photo"
-                src={ recipeProgress.drinks[0].strDrinkThumb }
-                width="400px"
-                alt=""
-              />
-              <h2 data-testid="recipe-title">{recipeProgress.drinks[0].strDrink}</h2>
-              <h4 data-testid="recipe-category">
-                {recipeProgress.drinks[0].strCategory}
-              </h4>
-              <ul>
-                {ingredientes.map((a2, i2) => (
-                  <li key={ i2 } data-testid={ `${i2}-ingredient-step` }>
-                    {`${a2} ${medidas[i2]}`}
-                  </li>
-                ))}
-              </ul>
-              <h5 data-testid="instructions">
-                {recipeProgress.drinks[0].strInstructions}
-              </h5>
+        recipeDetail.drinks && (
 
-              <button
-                type="button"
-                data-testid="finish-recipe-btn"
-              >
-                Finish Recipes
-              </button>
-              <button
-                type="button"
-                data-testid="share-btn"
-              >
-                Compartilhar
-              </button>
-              <button
-                type="button"
-                data-testid="favorite-btn"
-              >
-                Favoritar
-              </button> */}
-        </div>
+          <div>
+            <img
+              data-testid="recipe-photo"
+              src={ recipeDetail.drinks[0].strDrinkThumb }
+              width="400px"
+              alt=""
+            />
+            <h2 data-testid="recipe-title">{recipeDetail.drinks[0].strDrink}</h2>
+            <h4 data-testid="recipe-category">
+              {recipeDetail.drinks[0].strCategory}
+            </h4>
+            { recipeDetail.drinks && <Ingredientes />}
+            <h5 data-testid="instructions">
+              {recipeDetail.drinks[0].strInstructions}
+            </h5>
+
+            <button
+              type="button"
+              data-testid="finish-recipe-btn"
+            >
+              Finish Recipes
+            </button>
+            <button
+              type="button"
+              data-testid="share-btn"
+            >
+              Compartilhar
+            </button>
+            <button
+              type="button"
+              data-testid="favorite-btn"
+            >
+              Favoritar
+            </button>
+          </div>)
       )}
-      {console.log('foodDrinks é: ', foodDrinks, 'idDrink é: ', idRecipes)}
     </div>
   );
 }
