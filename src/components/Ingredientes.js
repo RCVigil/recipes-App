f348af1ca5e655fb191de76dff687da78264b4cc
Leonardo Zanette <leonardobzanette@gmail.com>
2022-08-31 11:52:06 -0300

    Req 40 -done, tem um bugzin de clicar e nao mudar o checked mas passa no teste e atualiza o LocalStora

--- src/components/Ingredientes.js
+++ src/components/Ingredientes.js
@@ -56,12 +56,60 @@ function Ingredientes() {
   const ingredientes = ingredient().filter((a) => a !== null && a !== '');
   const medidas = measure().filter((a) => a !== null && a !== '');
 
+  const checked = (i) => {
+    if (localStorage.getItem('inProgressRecipes') !== null && splited[1] === 'foods') {
+      const inProgress = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      return inProgress.meals[splited[2]]
+        ? inProgress.meals[splited[2]].some((a) => a === i) : false;
+    }
+    if (localStorage.getItem('inProgressRecipes') !== null && splited[1] === 'drinks') {
+      const inProgress = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      return inProgress.cocktails[splited[2]]
+        ? inProgress.cocktails[splited[2]].some((a) => a === i) : false;
+    }
+  };
+  const handleCheck = (ev, a) => {
+    if (ev.target.checked && splited[1] === 'foods') {
+      const inProgress = JSON.parse(localStorage.getItem('inProgressRecipes'))
+      || { cocktails: {},
+        meals: {} };
+      const inProgress2 = inProgress.meals[splited[2]]
+        ? [...inProgress.meals[splited[2]], a] : [a];
+      const newLocal = { cocktails: { ...inProgress.cocktails },
+        meals:
+      { ...inProgress.meals,
+        [splited[2]]: inProgress2,
+      } };
+      localStorage.setItem('inProgressRecipes', JSON.stringify(newLocal));
+      checked(a);
+    } else if (ev.target.checked && splited[1] === 'drinks') {
+      const inProgress = JSON.parse(localStorage.getItem('inProgressRecipes'))
+      || { cocktails: {},
+        meals: {} };
+      const inProgress2 = inProgress.cocktails[splited[2]]
+        ? [...inProgress.cocktails[splited[2]], a] : [a];
+      console.log(inProgress2);
+      const newLocal = { meals: { ...inProgress.meals },
+        cocktails:
+      { ...inProgress.cocktails,
+        [splited[2]]: inProgress2,
+      } };
+      localStorage.setItem('inProgressRecipes', JSON.stringify(newLocal));
+      checked(a);
+    }
+  };
+
+  checked('Tomat Puree');
   return (
     <ul>
       {ingredientes.map((a, i) => (
         <li key={ i } data-testid={ `${i}-ingredient-step` }>
           {`${a} ${medidas[i]}`}
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={ (ev) => handleCheck(ev, a) }
+            checked={ checked(a) }
+          />
         </li>
       ))}
     </ul>);
