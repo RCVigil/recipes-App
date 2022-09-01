468bfe5133bef6c878d6f06bf6b2f4ef8c019641
Leonardo Zanette <leonardobzanette@gmail.com>
2022-09-01 11:41:56 -0300

    Requisito 45 - DONE

--- src/components/CardDoneRecipes.js
+++ src/components/CardDoneRecipes.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import shareIcon from '../images/shareIcon.svg';
 
 // [{"id":"52771","type":"food","nationality":"Italian","category":"Vegetarian","alcoholicOrNot":"","name":"Spicy Arrabiata Penne","image":"https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg","doneDate":"23/06/2020","tags":["Pasta","Curry"]},{"id":"178319","type":"drink","nationality":"","category":"Cocktail","alcoholicOrNot":"Alcoholic","name":"Aquamarine","image":"https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg","doneDate":"23/06/2020","tags":[]}]
 
@@ -12,11 +13,23 @@ function CardDoneRecipes() {
 
       {recipesDone && recipesDone.map((a, i) => (
         <div key={ i }>
-          <img data-testid={ `${i}-horizontal-image` } src={ a.image } alt="" />
-          <p data-testid={ `${i}-horizontal-top-text` }>{a.category}</p>
+          <img
+            width="100px"
+            data-testid={ `${i}-horizontal-image` }
+            src={ a.image }
+            alt=""
+          />
+          <p data-testid={ `${i}-horizontal-top-text` }>
+            {`${a.nationality} - ${a.category}`}
+          </p>
           <p data-testid={ `${i}-horizontal-name` }>{a.name}</p>
           <p data-testid={ `${i}-horizontal-done-date` }>{a.doneDate}</p>
-          <button type="button" data-testid={ `${i}-horizontal-share-btn` }>Share</button>
+          <input
+            type="image"
+            data-testid={ `${i}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt=""
+          />
           {a.tags.map((ele, index) => (
             <p
               key={ index }
