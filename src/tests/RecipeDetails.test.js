b3964766e177e75affc43d5122aa93e9d9fa19e4
Peterson <petersonpaganelli@outlook.com>
2022-09-01 17:18:38 -0300

    Req 36 incompleto

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -8,54 +8,64 @@ import ReceitasProvider from '../Context/ReceitasProvider';
 import { arrabiataMock } from "./utils/arrabiataMock";
 import oneDrink from '../../cypress/mocks/oneDrink'
 
-describe('Testa o RecipeDetails', () => {
-    test('testa alguma coisa', async () => {
+describe('Testa a página RecipeDetails', () => {
+    test('testa o botão de favoritar(meals)', async () => {
         const { history } = renderWithRouter(<ReceitasProvider><App /></ReceitasProvider>)
         jest.spyOn(global, 'fetch').mockResolvedValue({
           json: jest.fn().mockResolvedValue(arrabiataMock)
         });
         history.push('/foods/52771');
-
         const favoriteBtn = await screen.findByTestId('favorite-btn');
         expect(favoriteBtn).toBeInTheDocument();
         userEvent.click(favoriteBtn);
         userEvent.click(favoriteBtn);     
       })
 
-      test('testa alguma coisa', async () => {
+      test('testa o botão de favoritar(drinks)', async () => {
         const { history } = renderWithRouter(<ReceitasProvider><App /></ReceitasProvider>)
         jest.spyOn(global, 'fetch').mockResolvedValue({
           json: jest.fn().mockResolvedValue(oneDrink)
         });
         history.push('/drinks/178319');
-
         const favoriteBtn = await screen.findByTestId('favorite-btn');
         expect(favoriteBtn).toBeInTheDocument();
         userEvent.click(favoriteBtn);
         userEvent.click(favoriteBtn);
       })
       
-      test('testa alguma coisa', async () => {
+      test('testa o caminho do arquivo(meals)', async () => {
+        const { history } = renderWithRouter(<ReceitasProvider><App /></ReceitasProvider>)
+        jest.spyOn(global, 'fetch').mockResolvedValue({
+          json: jest.fn().mockResolvedValue(arrabiataMock)
+        });
+        history.push('/foods/52771');
+        const startBtn = await screen.findByTestId('start-recipe-btn');
+        expect(startBtn).toBeInTheDocument();
+        userEvent.click(startBtn);
+        expect(history.location.pathname).toBe('/foods/52771/in-progress');
+      })
+
+      test('testa o caminho do arquivo(drinks)', async () => {
         const { history } = renderWithRouter(<ReceitasProvider><App /></ReceitasProvider>)
         jest.spyOn(global, 'fetch').mockResolvedValue({
           json: jest.fn().mockResolvedValue(oneDrink)
         });
         history.push('/drinks/178319');
-
         const startBtn = await screen.findByTestId('start-recipe-btn');
         expect(startBtn).toBeInTheDocument();
         userEvent.click(startBtn);
         expect(history.location.pathname).toBe('/drinks/178319/in-progress');
       })
 
-      test('testa alguma coisa', async () => {
+      test('testa o caminho do arquivo(meals)', async () => {
         const { history } = renderWithRouter(<ReceitasProvider><App /></ReceitasProvider>)
         jest.spyOn(global, 'fetch').mockResolvedValue({
-          json: jest.fn().mockResolvedValue(oneDrink)
+          json: jest.fn().mockResolvedValue(arrabiataMock)
         });
-        history.push('/drinks/178319');
-
-        const favoriteBtn = screen.getByTestId('0-recomendation-card');
-        expect(favoriteBtn).toBeInTheDocument();
+        history.push('/foods/52771');
+        const startBtn = await screen.findByTestId('start-recipe-btn');
+        expect(startBtn).toBeInTheDocument();
+        userEvent.click(startBtn);
+        expect(history.location.pathname).toBe('/foods/52771/in-progress');
       })
 });
